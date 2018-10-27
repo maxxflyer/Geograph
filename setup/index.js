0ebf9cbb3f9cd907800a50431a93c1b361d6d3d9
Wighawag <wighawag@gmail.com>
2018-10-27 14:50:51 +0100

    truffle migrate on setup

--- setup/index.js
+++ setup/index.js
@@ -1,49 +1,49 @@
 const Web3 = require('web3');
 
-// const path = require('path');
-// const fs = require('fs');
-// const childProcess = require('child_process');
+const path = require('path');
+const fs = require('fs');
+const childProcess = require('child_process');
 
-// function getContractInfos(contractInfosFolder, networkId) {
-//     const contractInfos = {};
-//     fs.readdirSync(contractInfosFolder).forEach(file => {
-//         const contractInfo = JSON.parse(fs.readFileSync(path.join(contractInfosFolder,file)).toString());
-//         const networkKeys = Object.keys(contractInfo.networks);
-//         if(!networkId) {
-//             if(networkKeys && networkKeys.length > 0) {
-//                 networkId = networkKeys[0];
-//                 console.log('no networkId specified, using networkId', networkId, 'from ', JSON.stringify(contractInfo.networks));
-//             } else {
-//                 console.error('no networkId specified, and can\'t find one');
-//             }
-//         }
-//         if (networkId && contractInfo.networks[networkId]){
-//             const currentNetwork = contractInfo.networks[networkId];
-//             contractInfo.networks = {}; // remove other networks info // TODO not sure why they are there ?
-//             contractInfo.networks[networkId] = currentNetwork;
-//             contractInfos[contractInfo.contractName] = contractInfo;
-//         }
-//     })
+function getContractInfos(contractInfosFolder, networkId) {
+    const contractInfos = {};
+    fs.readdirSync(contractInfosFolder).forEach(file => {
+        const contractInfo = JSON.parse(fs.readFileSync(path.join(contractInfosFolder,file)).toString());
+        const networkKeys = Object.keys(contractInfo.networks);
+        if(!networkId) {
+            if(networkKeys && networkKeys.length > 0) {
+                networkId = networkKeys[0];
+                console.log('no networkId specified, using networkId', networkId, 'from ', JSON.stringify(contractInfo.networks));
+            } else {
+                console.error('no networkId specified, and can\'t find one');
+            }
+        }
+        if (networkId && contractInfo.networks[networkId]){
+            const currentNetwork = contractInfo.networks[networkId];
+            contractInfo.networks = {}; // remove other networks info // TODO not sure why they are there ?
+            contractInfo.networks[networkId] = currentNetwork;
+            contractInfos[contractInfo.contractName] = contractInfo;
+        }
+    })
 
-//     return {networkId, contractInfos};
-// }
+    return {networkId, contractInfos};
+}
 
-// function deployContracts(truffleNetwork, networkId) {
-//     console.log('deploying contracts to network ' + networkId);
-//     return new Promise((resolve, reject) => {
-//         var child = childProcess.exec('yarn truffle migrate --reset --network ' + truffleNetwork, {
-//             cwd: '../contracts'
-//         })
-//         child.stdout.pipe(process.stdout)
-//         child.on('exit', function() {
-//             //TODO use truffle folder as above OR tmp_deployments OR deployments
-//             // const contractInfosFolder = '../contracts/build/contracts';
-//             // + '/' + 'tmp_deployments'
-//             // fs.rmdirSync(contractInfosFolder);
-//             resolve(getContractInfos('../contracts/tmp_deployments', networkId));
-//         })
-//     });
-// }
+function deployContracts(truffleNetwork, networkId) {
+    console.log('deploying contracts to network ' + networkId);
+    return new Promise((resolve, reject) => {
+        var child = childProcess.exec('yarn truffle migrate --reset --network ' + truffleNetwork, {
+            cwd: './contracts'
+        })
+        child.stdout.pipe(process.stdout)
+        child.on('exit', function() {
+            //TODO use truffle folder as above OR tmp_deployments OR deployments
+            // const contractInfosFolder = '../contracts/build/contracts';
+            // + '/' + 'tmp_deployments'
+            // fs.rmdirSync(contractInfosFolder);
+            resolve(getContractInfos('./contracts/build/contracts', networkId));
+        })
+    });
+}
 
 const args = process.argv;
 const nodeUrl = args[2] || "http://localhost:8545";
@@ -65,49 +65,36 @@ function backoff(retries, func, delay = 0.5, multiplier = 2) {
     );
 }
 
+let accounts;
+let networkId;
+
 function setup(){
     return backoff(10, web3.eth.net.getId, 3, 1)
         .then((id) => {
             networkId = id;
             return web3.eth.getAccounts();
-        }) 
+        })
+        .then((acc) => {accounts = acc})
+        .then(() => deployContracts('ganache', networkId))
         .then(run);
 }
 
-async function run(accounts) {
-    const owner = web3.utils.toChecksumAddress(accounts[0]);
-    const squatter = web3.utils.toChecksumAddress(accounts[1]);
+async function run({networkId, contractInfos}) {
+    const contracts = {};
+    for(const contractInfoName of Object.keys(contractInfos)) {
+        const ContractInfo = contractInfos[contractInfoName];
+        console.log('CONTRACT ' + contractInfoName + ' DEPLOYED AT : ' + ContractInfo.networks[networkId].address);
+        contracts[contractInfoName] = new web3.eth.Contract(ContractInfo.abi, ContractInfo.networks[networkId].address);
+    }
 
-    const zero = '0x0000000000000000000000000000000000000000000000000000000000000000';
-    
-    const ens = await new web3.eth.Contract(ENSContractABI)
-        .deploy({data:ENSRegistryByteCode})
-        .send({from: owner, gas});
+    // const domainToOwn = ['myname', rootDomain];
+    // await ens.methods.setSubnodeOwner(zero, web3.utils.sha3(rootDomain), registrar.options.address).send({from: owner, gas});
+    // await registrar.methods.register(web3.utils.sha3(domainToOwn[0]), squatter).send({from: squatter, gas});
 
-    console.log('ENS REGISTRY DEPLOYED AT : ' + ens.options.address);
+    // const registeredOwner = await ens.methods.owner(namehash.hash(domainToOwn.join('.'))).call();
+    // console.log('registered owner of : ' + domainToOwn.join('.'), registeredOwner);
 
-    const publicResolver = await new web3.eth.Contract(PublicResolverABI)
-        .deploy({data:PublicResolverByteCode})
-        .send({from: owner, gas});
-
-    console.log('PUBLIC RESOLVER DEPLOYED AT : ' + publicResolver.options.address);
-
-    const rootDomain = 'geo';
-
-    const registrar = await new web3.eth.Contract(FIFSRegistrarContractABI)
-        .deploy({data:FIFSRegistrarByteCode, arguments:[ens.options.address, namehash.hash(rootDomain), zero]})
-        .send({from:owner, gas});
-
-    console.log('FIFS REGISTRAR DEPLOYED AT : ' + registrar.options.address);
-
-    const domainToOwn = ['myname', rootDomain];
-    await ens.methods.setSubnodeOwner(zero, web3.utils.sha3(rootDomain), registrar.options.address).send({from: owner, gas});
-    await registrar.methods.register(web3.utils.sha3(domainToOwn[0]), squatter).send({from: squatter, gas});
-
-    const registeredOwner = await ens.methods.owner(namehash.hash(domainToOwn.join('.'))).call();
-    console.log('registered owner of : ' + domainToOwn.join('.'), registeredOwner);
-
-    await ens.methods.setResolver(namehash.hash('myname.geo'), publicResolver.options.address).send({from: squatter});
+    // await ens.methods.setResolver(namehash.hash('myname.geo'), publicResolver.options.address).send({from: squatter});
 }
 
 setup()
